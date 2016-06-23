678ef7d549e7c52c910e8a3c45a65555aef37150
jiheim <koyote.nowhere@gmail.com>
2016-06-23 16:19:01 +0900

    0623

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+
+
+$('.intro-menu').prepend('<button class="mobile-gnb-open"><i class="fa fa-bars" aria-hidden="true"></i></button>')
+
+var winWidth = 0; 
+$(window).resize(function() {
+ winWidth = $(document).width();
+});
+
+
+if(winWidth < 767){
+	$('.mobile-gnb-open').click(function(){
+
+		$('.intro-menu').prepend('<button class="mobile-gnb-close"><i class="fa fa-times" aria-hidden="true"></i></button>');
+
+		$('.intro-menu-list').animate({
+			top : "0"
+		},"slow", "swing");
+
+		$('.mobile-gnb-close').click(function(){
+			$('.mobile-gnb-close').hide();
+			$('.mobile-gnb-open').show();
+
+			$('.intro-menu-list').animate({
+				top : "-1000px"
+			},"slow", "swing");
+		});
+
+	})
+
+}else{
+	$('.mobile-gnb-open').unbind('click');
+	$('.intro-menu-list').css('top','97vh');
+}
+
+		
+
