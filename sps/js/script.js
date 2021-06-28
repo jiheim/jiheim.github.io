061e9070b743a13e32d4ced80728dcc0e36221c9
Jihe <koyote.nowhere@gmail.com>
2021-06-28 23:05:55 +0900

    개고생....

---
+++ sps/js/script.js
@@ -0,0 +1,71 @@
+
+
+
+
+
+    // Hide Header on on scroll down
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('nav ul').outerHeight();
+
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+        
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+        
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight){
+            // Scroll Down
+            $('nav').removeClass('nav-down').addClass('nav-up');
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                $('nav').removeClass('nav-up').addClass('nav-down');
+            }
+        }
+        
+        lastScrollTop = st;
+    }
+
+
+
+
+
+  //initiating jQuery  
+  jQuery(function($) {
+    $(document).ready( function() {
+      //enabling stickUp on the '.navbar-wrapper' class
+      $('.navbar-wrapper').stickUp({
+
+                    parts: {
+                      0:'practice',
+                      1:'edit-lyrics',
+                      2:'adjust-sync',
+                      3:'recording',
+                      4:'clip',
+                      5:'clip-sync',
+                      6:'template'
+                    },
+                    itemClass: 'menuItem',
+                    itemHover: 'active'
+                  });
+    });
+  });
+
+
+  
