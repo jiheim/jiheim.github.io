ed198cd7bdf3e2266b4918962b0e00d2f377962d
Jihe <koyote.nowhere@gmail.com>
2021-07-09 15:01:49 +0900

    동영상 스크롤 제어

--- sps/js/script.js
+++ sps/js/script.js
@@ -45,7 +45,7 @@
 
 
 
-
+//메뉴 선택
   //initiating jQuery  
   jQuery(function($) {
     $(document).ready( function() {
@@ -68,3 +68,29 @@
   });
 
 
+
+
+
+//동영상 스크롤 제어
+var players = []
+$('.video-preview').each(function() {
+  players.push({
+      player: new Vimeo.Player($(this).find("iframe").get(0)),
+      top: $(this).position().top,
+      status: "paused"
+  })
+});
+var viewportHeight = $(window).height();
+
+$(window).on('scroll', function() {
+    var scrollPos = $(window).scrollTop();
+    for(var i=0; i<players.length;i++) {
+      var elementFromTop = players[i].top - scrollPos;
+      var status = (elementFromTop > 0 && elementFromTop < players[i].top + viewportHeight) ? "play" : "pause";   
+      if(players[i].status != status) {
+          players[i].status = status;
+          players[i].player[status]();
+          console.log(i, status);      
+      }
+    }
+});
