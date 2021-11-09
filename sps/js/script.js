dfbd1ff0c75e9d9cbcd82a350fcb40472d180f45
Jihe <koyote.nowhere@gmail.com>
2021-11-09 19:32:35 +0900

    Share 추가

--- sps/js/script.js
+++ sps/js/script.js
@@ -2,49 +2,6 @@
 
 
 
-
-    // Hide Header on on scroll down
-    var didScroll;
-    var lastScrollTop = 0;
-    var delta = 5;
-    var navbarHeight = $('nav ul').outerHeight();
-
-    $(window).scroll(function(event){
-        didScroll = true;
-    });
-
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-        }
-    }, 250);
-
-    function hasScrolled() {
-        var st = $(this).scrollTop();
-        
-        // Make sure they scroll more than delta
-        if(Math.abs(lastScrollTop - st) <= delta)
-            return;
-        
-        // If they scrolled down and are past the navbar, add class .nav-up.
-        // This is necessary so you never see what is "behind" the navbar.
-        if (st > lastScrollTop && st > navbarHeight){
-            // Scroll Down
-            $('nav').removeClass('nav-down').addClass('nav-up');
-        } else {
-            // Scroll Up
-            if(st + $(window).height() < $(document).height()) {
-                $('nav').removeClass('nav-up').addClass('nav-down');
-            }
-        }
-        
-        lastScrollTop = st;
-    }
-
-
-
-
 //메뉴 선택
   //initiating jQuery  
   jQuery(function($) {
@@ -62,7 +19,8 @@
                       6:'clip',
                       7:'trim',
                       8:'clip-sync',
-                      9:'template'
+                      9:'template',
+                      10:'share'
                     },
                     itemClass: 'menuItem',
                     itemHover: 'active',
